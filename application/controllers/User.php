8911fa9df29e6eebf9efb9d95750fa9ebf7c95a1
Chairulhamid <chairulhamid01@gmail.com>
2020-11-18 06:40:39 +0700

    merapikan tampilan controler

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -25,13 +25,9 @@ class User extends CI_Controller
     {
         $data['title'] = 'Edit Profile';
         $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
-
         $this->form_validation->set_rules('name', 'full Name', 'required|trim');
 
         if ($this->form_validation->run() == false) {
-
-
-
             $this->load->view('templates/header', $data);
             $this->load->view('templates/sidebar', $data);
             $this->load->view('templates/topbar', $data);
@@ -44,13 +40,10 @@ class User extends CI_Controller
             //cek jika ada gambar yang akan di upoad
             $upload_image = $_FILES['image']['name'];
 
-
-
             if ($upload_image) {
                 $config['allowed_types'] = 'gif|jpg|png';
                 $config['maz_size'] = '2048';
                 $config['upload_path'] = './assets/img/profile/';
-
                 $this->load->library('upload', $config);
 
                 if ($this->upload->do_upload('image')) {
@@ -58,16 +51,12 @@ class User extends CI_Controller
                     if ($old_image != 'default.jpg') {
                         unlink(FCPATH . 'assets/img/profile/' . $old_image);
                     }
-
-
                     $new_image = $this->upload->data('file_name');
                     $this->db->set('image', $new_image);
                 } else {
                     echo $this->upload->display_errors();
                 }
             }
-
-
             $this->db->set('name', $name);
             $this->db->where('email', $email);
             $this->db->update('user');
